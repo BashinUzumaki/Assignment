b0602fe5ff636fba109100e3e43ec530923bdf4e
BashinUzumaki <130160019+BashinUzumaki@users.noreply.github.com>
2023-04-07 19:47:55 +0500

    Add files via upload

---
+++ assignemnt/prompt.js
@@ -0,0 +1,122 @@
+// /// Question#1 ///// 
+// let Q1 = +prompt("Enter First Number")
+// console.log("First Number = " + Q1)
+// let Q2 = +prompt("Enter Second Number")
+// console.log("Second Number = " + Q2)
+// if (Q1 > Q2) {
+//     console.log(" first entry is bigger = " + Q1)
+// }
+// else if (Q2 > Q1) {
+//     console.log("second entry is bigger = " + Q2)
+// } else {
+//     console.log("equal")
+// }
+// ///// Question#2 /////
+// let numberGiven = +prompt("Enter a Number")
+// console.log("First entry : " + numberGiven)
+// let l1 = ("The sign is positive + ")
+// let l2 = ("The sign is negative - ")
+// if (numberGiven > 0) {
+//     alert(l1)
+// }
+// else if(numberGiven < 0)
+// {
+//     alert(l2)
+// } else { 
+//     alert("error")
+// }
+//     // Question#3////
+//     let q1 = +prompt("Enter First Number")
+//     console.log("First Number = " + q1)
+//     let q2 = +prompt("Enter Second Number")
+//     console.log("Second Number = " + q2)
+//     let q3 = +prompt("Enter third Number")
+//     console.log("Third Number = " + q3)
+//     let q4 = +prompt("Enter fourth Number")
+//     console.log("Fourth Number = " + q4)
+//     let q5 = +prompt("Enter fifth Number")
+//     console.log("Fifth Number = " + q5)
+//    max = q1
+//    if(q2>max ){
+//    max = q2
+//    }
+//    if(q3>q2){
+//     max = q3
+//    }
+//    if(q4>q3){
+//     max = q4
+//    } 
+//    if(q5>q4)
+//    {
+//     max = q5
+//    }
+//    console.log("higgest value out of them : " + max)
+//    let max1 = Math.max(q1,q2,q3,q4,q5)
+//  document.write(max)
+// ///question #4//////
+// let e1 = ("even")
+// let o1 = ("odd")
+// for(let i = 0; i<=15; i++ ){
+// if(i%2===0){
+//     console.log(i + " is " + e1)
+// }
+// else if (i%2 !== 0){
+//     console.log(i + " is " + o1 )
+// }
+// else{console.log("error")}
+// }
+// /question#5///
+// let Question_1 = ("Enter your name")
+// let Intro = prompt("Welcome User , "+ Question_1)
+// let mark1 = +prompt("Enter marks accquired in Maths : ( Out of 20 ) ")
+// let mark2 = +prompt("Enter marks accquired in Biology : (Out of 20 ) ")
+// let mark3 = +prompt("Enter marks accquired in Chemistry : (out of 2 ) ")
+// let mark4 = +prompt("Enter marks accquired in Urdu : ( Out of 20 ) ")
+// let mark5 = +prompt("Enter marks accquired in Islamiat : ( Out of 20 ) ")
+// mark1 = mark1 =>20
+// mark2 = mark2 =>20
+// mark3 = mark3 =>20
+// mark4 = mark4 =>20
+// mark5 = mark5 =>20
+
+// let total = (mark1 + mark2 + mark3 + mark4 + mark5)
+// let g1 = ("F grade")
+// let g2 = ("D grade")
+// let g3 = ("C grade")
+// let g4 = ("B grade")
+// let g5 = ("A grade")
+// if (total < 60) {
+//     alert(g1)
+// } if (total > 60 && total < 70) {
+//     alert(g2)
+// }
+// if (total>70 && total<80){
+//     alert(g3)
+// }if(total>80 && total<90){
+//     alert(g4)
+// }if(total>90 && total<100){
+//     alert(g5)
+// }else if (mark1 , mark2 , mark3 , mark4 , mark5){
+//     alert("Invalid Marks")
+// }
+// /////-Question#6-///////
+// let code = ("fizzbuzz")
+// let code_1 = ("fizz")
+// let code_2 = ("buzz")
+// for (let k = 1; k <= 100; k++){
+//     if (k % 3 === 0 && k % 5 === 0) {
+//         console.log(code)
+//     }
+//  if (k % 3 === 0){
+//     console.log(code_1 )
+// }if(k % 5 === 0)
+//     console.log(code_2) 
+//   else (console.log(i))}
+///Question#7////
+// for(let i =1 ; i<= 5; i++){
+//     let style =""
+
+// for(let j = 1 ; j<=i ;j++){
+//     style += "*";
+// }
+// console.log(style)}
